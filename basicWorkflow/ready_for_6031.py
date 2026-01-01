a90f0628c270b0baa6cbea951d0f9d6b1352e011
Andria Bao <58490077+andriabao@users.noreply.github.com>
2026-01-01 14:41:54 -0500

    Add files via upload

---
+++ basicWorkflow/ready_for_6031.py
@@ -0,0 +1,14 @@
+def is_code_good(safe_from_bugs, ready_for_change, easy_to_understand):
+    """
+    Determines whether a piece of software meets 6.031 standards.
+    Software meets 6.031 standards if it is safe from bugs, ready for change, and easy to understand.
+    """
+    return safe_from_bugs and ready_for_change and easy_to_understand
+
+
+############ TESTS ###############
+for sfb in (True, False):
+    for rfc in (True, False):
+        for etu in (True, False):
+            assert is_code_good(sfb, rfc, etu) == (sfb and rfc and etu)
+print("All tests passed!")
